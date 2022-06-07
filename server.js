b36ed93fd31cb28b2a64b071713445365e212fd3
Zachary Baumgarten <zach.baumgarten@yahoo.com>
2022-06-07 18:58:50 -0500

    heroku

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('HERRO!')
+})
+
+const PORT = 8080
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`))
